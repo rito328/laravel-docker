046dae7d625a8e4584d30209d3d7524e824b5faf
rito328 <ritolabo@gmail.com>
2019-07-31 00:15:52 +0900

    Changes init DuskTestCase

--- docker/app/init/DuskTestCase.php
+++ docker/app/init/DuskTestCase.php
@@ -6,10 +6,25 @@ use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->app = $this->createApplication();
+        $this->app->loadEnvironmentFrom('.env.dusk.local');
+        Artisan::call('config:cache');
+    }
+
+    public function tearDown(): void
+    {
+        Artisan::call('config:clear');
+        parent::tearDown();
+    }
 
     /**
      * Prepare for Dusk test execution.
@@ -38,8 +53,8 @@ abstract class DuskTestCase extends BaseTestCase
         return RemoteWebDriver::create(
             'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
             ChromeOptions::CAPABILITY,
-            $options
-        )
+                $options
+            )
         );
 
     }
